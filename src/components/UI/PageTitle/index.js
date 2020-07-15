0c0329c398a9a87f0f1e4a2842fd90a48b14773e
Andre Teles <andre.telestp@gmail.com>
2020-07-15 19:26:32 -0300

    added component PageTitle

--- src/components/UI/PageTitle/index.js
+++ src/components/UI/PageTitle/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Title, Subtitle } from './styles';
+import { Container } from './styles';
 
 function PageTitle({ Icon, title, subtitle }) {
-    return <div />;
+    return (
+        <>
+            <Container>
+                <Icon />
+                <Title>{title}</Title>
+            </Container>
+
+            <Subtitle>{subtitle}</Subtitle>
+        </>
+    );
 }
 PageTitle.propTypes = {
     Icon: PropTypes.string,
--- src/components/UI/PageTitle/styles.js
+++ src/components/UI/PageTitle/styles.js
@@ -1,10 +1,22 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+
+    > svg {
+        font-size: 1.8rem;
+    }
+`;
+
 export const Title = styled.h1`
     margin: 0;
+    font-size: 2rem;
+    margin-left: 0.5rem;
 `;
 export const Subtitle = styled.h2`
     color: #777;
     margin-top: 0.5rem;
     font-size: 1.3rem;
+    border-bottom: 1px solid #dedede;
 `;
