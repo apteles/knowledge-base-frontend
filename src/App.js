8783222370231b06aa0d1fc1408fae731b23bc65
Andre Teles <andre.telestp@gmail.com>
2020-07-28 21:21:48 -0300

    started crud users

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import GlobalStyles from './styles/global';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from './styles/global';
 import './config/reactotron';
 import Routes from './routes';
 import { store } from './store';
+
 function App() {
     return (
         <Provider store={store}>
--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,15 +1,54 @@
 import React from 'react';
-import PageTitle from '../../components/UI/PageTitle';
+import { Tab, Row, Col, Nav } from 'react-bootstrap';
 import { FaCogs } from 'react-icons/fa';
+import PageTitle from '../../components/UI/PageTitle';
 // import { Container } from './styles';
 
 function Admin() {
     return (
-        <PageTitle
-            icon={FaCogs}
-            title="Administração"
-            subtitle="Cadastros Gerais"
-        />
+        <>
+            <PageTitle
+                icon={FaCogs}
+                title="Administração"
+                subtitle="Cadastros Gerais"
+            />
+
+            <Tab.Container>
+                <Row>
+                    <Col sm={1}>
+                        <Nav variant="pills" className="flex-column">
+                            <Nav.Item>
+                                <Nav.Link eventKey="third">Artigos</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="second">
+                                    Categorias
+                                </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="first">Usuários</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Col>
+                    <Col sm={9}>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="first" />
+                            <Tab.Pane eventKey="second">
+                                <p>
+                                    Lorem ipsum dolor sit amet consectet
+                                    adipisicing elit. Corrupti doloribus
+                                    voluptate accusantium earum nisi. Totam,
+                                    maxime provident iusto optio est distinctio
+                                    obcaecati! Ullam exercitationem nobis minus
+                                    numquam placeat quaerat officiis.
+                                </p>
+                            </Tab.Pane>
+                            <Tab.Pane eventKey="third" />
+                        </Tab.Content>
+                    </Col>
+                </Row>
+            </Tab.Container>
+        </>
     );
 }
 
